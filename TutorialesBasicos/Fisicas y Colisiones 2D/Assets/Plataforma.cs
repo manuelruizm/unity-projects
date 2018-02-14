1658fbd0df03087085489e6cdc9e3ba3b4a7cc90
manuelruizm <ruizmaldonadomanuel@gmail.com>
2018-02-14 14:27:44 +0100

    Físicas y colisiones en 2D

---
+++ TutorialesBasicos/Fisicas y Colisiones 2D/Assets/Plataforma.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plataforma : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		print("Ha ocurrido un evento Collision!");
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		print("Ha ocurrido un evento Trigger!");
+	}
+}
