2b89d53ffa71fd042d3b8501a6dc009e56e561e9
manuelruizm <ruizmaldonadomanuel@gmail.com>
2018-02-14 13:58:38 +0100

    Manejo de escenas

---
+++ TutorialesBasicos/Manejo de Escenas/Assets/Controlador.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controlador : MonoBehaviour {
+
+	public void CambiarEscena(string nombre){
+		SceneManager.LoadScene(nombre);
+	}
+
+	public void Salir(){
+		print("Saliendo del juego");
+		Application.Quit();
+	}
+}
