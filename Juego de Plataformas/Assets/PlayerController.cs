d49ec18b773e954cc24f0ec85818326d29b2f8f1
manuelruizm <ruizmaldonadomanuel@gmail.com>
2018-02-24 13:19:59 +0100

    Colliders y movimiento horizontal del player

---
+++ Juego de Plataformas/Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float maxSpeed = 5f;
+	public float speed;
+
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate()
+	{
+		float h = Input.GetAxis("Horizontal");
+		rb2d.AddForce(Vector2.right * speed * h);
+
+		if(rb2d.velocity.x > maxSpeed){
+			rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
+		}
+		if(rb2d.velocity.x < -maxSpeed){
+			rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);
+		}
+
+		Debug.Log(rb2d.velocity.x);
+	}
+}
