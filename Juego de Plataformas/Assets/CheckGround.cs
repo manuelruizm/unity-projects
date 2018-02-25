1aadbb5743cf3360cc7e59f66ecf6c5249804d4c
manuelruizm <ruizmaldonadomanuel@gmail.com>
2018-02-25 13:11:43 +0100

    Gestión de animaciones desde script

---
+++ Juego de Plataformas/Assets/CheckGround.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGround : MonoBehaviour {
+
+	private PlayerController player;
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponentInParent<PlayerController>();
+	}
+	
+	void OnCollisionStay2D(Collision2D collisionInfo)
+	{
+		if(collisionInfo.gameObject.tag == "Ground"){
+			player.grounded = true;
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D collisionInfo)
+	{
+		if(collisionInfo.gameObject.tag == "Ground"){
+			player.grounded = false;
+		}
+	}
+}
--- Juego de Plataformas/Assets/Scripts/PlayerController.cs
+++ Juego de Plataformas/Assets/Scripts/PlayerController.cs
@@ -6,18 +6,22 @@ public class PlayerController : MonoBehaviour {
 
 	public float maxSpeed = 5f;
 	public float speed;
+	public bool grounded;
 
 	private Rigidbody2D rb2d;
+	private Animator anim;
 
 	// Use this for initialization
 	void Start () {
 		
 		rb2d = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		anim.SetFloat("Speed", Mathf.Abs(rb2d.velocity.x));
+		anim.SetBool("Grounded", grounded);
 	}
 
 	void FixedUpdate()
@@ -25,13 +29,9 @@ public class PlayerController : MonoBehaviour {
 		float h = Input.GetAxis("Horizontal");
 		rb2d.AddForce(Vector2.right * speed * h);
 
-		if(rb2d.velocity.x > maxSpeed){
-			rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
-		}
-		if(rb2d.velocity.x < -maxSpeed){
-			rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);
-		}
-
+		float limitedSpeed = Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed);
+		rb2d.velocity = new Vector2(limitedSpeed, rb2d.velocity.y);
+		
 		Debug.Log(rb2d.velocity.x);
 	}
 }
