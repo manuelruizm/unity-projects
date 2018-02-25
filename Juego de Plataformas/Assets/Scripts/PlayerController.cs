6c860a01207dea13715346b68ee8e4f9f41e3e7e
manuelruizm <ruizmaldonadomanuel@gmail.com>
2018-02-25 13:16:45 +0100

    Cambio de orientación hacia donde mira el player - localScale

--- Juego de Plataformas/Assets/Scripts/PlayerController.cs
+++ Juego de Plataformas/Assets/Scripts/PlayerController.cs
@@ -31,6 +31,14 @@ public class PlayerController : MonoBehaviour {
 
 		float limitedSpeed = Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed);
 		rb2d.velocity = new Vector2(limitedSpeed, rb2d.velocity.y);
+
+		if(h > 0.1f){
+			transform.localScale = new Vector3(1f, 1f, 1f);
+		}
+
+		if(h < -0.1f){
+			transform.localScale = new Vector3(-1f, 1f, 1f);
+		}
 		
 		Debug.Log(rb2d.velocity.x);
 	}
