a6df3053b246c3a074271e5c289b08a72ba45b3e
manuelruizm <ruizmaldonadomanuel@gmail.com>
2018-03-01 13:38:04 +0100

    Primera plataforma con movimiento - Vector3.MoveTowards

--- Juego de Plataformas/Assets/Scripts/DrawSceneLine.cs
+++ Juego de Plataformas/Assets/Scripts/DrawSceneLine.cs
@@ -11,6 +11,8 @@ public class DrawSceneLine : MonoBehaviour {
 		if(from != null && to != null){
 			Gizmos.color = Color.cyan;
 			Gizmos.DrawLine(from.position, to.position);
+			Gizmos.DrawSphere(from.position, 0.15f);
+			Gizmos.DrawSphere(to.position, 0.15f);
 		}
 	}
 }
