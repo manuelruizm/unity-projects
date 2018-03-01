dc36c056dcc735f42facb2b54af9cf908e8c4b1a
manuelruizm <ruizmaldonadomanuel@gmail.com>
2018-03-01 13:44:49 +0100

    Plataforma con movimiento vertical

--- Juego de Plataformas/Assets/Scripts/CheckGround.cs
+++ Juego de Plataformas/Assets/Scripts/CheckGround.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class CheckGround : MonoBehaviour {
 
 	private PlayerController player;
+	private Rigidbody2D rb2d;
 
 	// Use this for initialization
 	void Start () {
 		player = GetComponentInParent<PlayerController>();
+		rb2d = GetComponentInParent<Rigidbody2D>();
+	}
+
+	void OnCollisionEnter2D(Collision2D collisionInfo)
+	{
+		if(collisionInfo.gameObject.tag == "Platform"){
+			rb2d.velocity = new Vector3(0f, 0f, 0f);
+			player.transform.parent = collisionInfo.transform;
+			player.grounded = true;
+		}
 	}
 	
 	void OnCollisionStay2D(Collision2D collisionInfo)
@@ -18,6 +29,7 @@ public class CheckGround : MonoBehaviour {
 		}
 
 		if(collisionInfo.gameObject.tag == "Platform"){
+			player.transform.parent = collisionInfo.transform;
 			player.grounded = true;
 		}
 	}
@@ -29,6 +41,7 @@ public class CheckGround : MonoBehaviour {
 		}
 
 		if(collisionInfo.gameObject.tag == "Platform"){
+			player.transform.parent = null;
 			player.grounded = false;
 		}
 	}
