47dbfbe00893005e947c33a77704ad614341eee7
manuelruizm <ruizmaldonadomanuel@gmail.com>
2018-03-07 21:26:58 +0100

    Inicio del proyecto

---
+++ TutorialesIntermedios/Variables y funciones entre Scripts/Assets/ScriptA.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptA : MonoBehaviour {
+
+	public int numero = 1;
+
+	public void MostrarNumero(){
+		print(numero);
+	}
+}
