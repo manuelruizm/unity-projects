a73ce358002518e4171f4aa8647d70890a02d10e
manuelruizm <ruizmaldonadomanuel@gmail.com>
2018-03-08 16:57:02 +0100

    Cargar y guardar un objeto en un fichero json

--- TutorialesIntermedios/SerializacionJSON/Assets/EjemploJSON.cs
+++ TutorialesIntermedios/SerializacionJSON/Assets/EjemploJSON.cs
@@ -1,8 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class EjemploJSON : MonoBehaviour {
+
+	string filePath;
+	string jsonString;
+
+	void Awake()
+	{
+		filePath = Application.dataPath + "/Personaje.json";
+		jsonString = File.ReadAllText(filePath);
+		Personaje personaje = JsonUtility.FromJson<Personaje>(jsonString);
+		print(personaje);
+
+		personaje.nivel = 50;
+		jsonString = JsonUtility.ToJson(personaje);
+		File.WriteAllText(filePath, jsonString);
+	}
 
 	// Use this for initialization
 	void Start () {
@@ -12,5 +28,16 @@ public class EjemploJSON : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+}
+
+[System.Serializable]
+public class Personaje {
+	public string nombre;
+	public string profesion;
+	public int nivel;
+
+	public override string ToString(){
+		return string.Format("{0}: {1} nivel {2}", nombre, profesion, nivel);
 	}
 }
