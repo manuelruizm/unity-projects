b499869e968d7ee12484330cdc42ce4241f258ae
manuelruizm <ruizmaldonadomanuel@gmail.com>
2018-03-11 12:43:54 +0100

    Implementación de pseudo singleton en el método Awake

---
+++ TutorialesIntermedios/Singleton/Assets/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+
+	void Awake()
+	{
+		if(instance == null) instance = this;
+		else if(instance != this) Destroy(gameObject);
+		DontDestroyOnLoad(this);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetKeyDown("1")){
+			print("Cambiando a escena Principal");
+			SceneManager.LoadScene("Principal");
+		}
+		else if(Input.GetKeyDown("2")){
+			print("Cambiando a escena Juego");
+			SceneManager.LoadScene("Juego");
+		}
+		else if(Input.GetKeyDown("3")){
+			print("Cambiando a escena Créditos");
+			SceneManager.LoadScene("Creditos");
+		}
+	}
+}
