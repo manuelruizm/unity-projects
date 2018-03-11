b5a8d3fc1245b9d639301210d97d505575ee39c4
manuelruizm <ruizmaldonadomanuel@gmail.com>
2018-03-11 12:53:34 +0100

    Mover un objeto hacia el puntero al hacer click

---
+++ TutorialesIntermedios/Mover objeto a puntero/Assets/Seguimiento.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seguimiento : MonoBehaviour {
+
+	public float speed;
+
+	Vector3 target;
+
+	// Use this for initialization
+	void Start () {
+		target = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetMouseButtonDown(0)){
+			target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			target.z = 0f;
+		}
+
+		transform.position = Vector3.MoveTowards(transform.position, target, speed*Time.deltaTime);
+
+		Debug.DrawLine(transform.position, target, Color.green);
+	}
+}
